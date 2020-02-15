b9006e7c6e43838ae941fb04bf9e9dc8af253900
501 <501@academicos.cesde.edu.co>
2020-02-14 21:11:31 -0500

    esta es la version 1 de mi codigo

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+//este es un comentario de una linea
+
+
+
+/*Este es un 
+comentario de 
+varias lineas*/
+
+$nombre="yuliana"; //los nombres siempre van en comillas//
+echo($nombre);
+$edad=32;
+echo($edad);
+$estatura=1.53;
+echo($estatura);
+$nombre="isabella";
+echo($nombre);
+
+//*$edad=105;
+echo($edad);
+
+echo("hola mundo ");
+print("<br>"); //salto de linea//
+echo("mi nombre es: ".$nombre. " y la edad es: " .$edad=15);
+
+
+
+
+
+?>
